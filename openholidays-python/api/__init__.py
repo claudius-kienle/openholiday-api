a7c4ef4383605b8ec396d1e10527cde9da87daa7
Claudius Kienle <privat@claudiuskienle.de>
2025-11-10 19:19:33 +0100

    fix

--- openholidays-python/api/__init__.py
+++
@@ -1,7 +0,0 @@
-# flake8: noqa
-
-# import apis into api package
-from openholidays-python.api.holidays_api import HolidaysApi
-from openholidays-python.api.regional_api import RegionalApi
-from openholidays-python.api.statistics_api import StatisticsApi
-
